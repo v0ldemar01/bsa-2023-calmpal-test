adc4aa1ac76bccb88f79b99dff25e4590871c0e0
Анастасія Лисенко <anastasiialysenko0@gmail.com>
2023-08-21 21:13:09 +0300

    cp-19: + on page refresh token check

--- frontend/src/libs/components/app/app.tsx
+++ frontend/src/libs/components/app/app.tsx
@@ -1,12 +1,29 @@
 import reactLogo from '#assets/img/react.svg';
 import { Header, Link, RouterOutlet } from '#libs/components/components.js';
 import { AppRoute } from '#libs/enums/enums.js';
-import { useAppDispatch, useEffect, useLocation } from '#libs/hooks/hooks.js';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useEffect,
+  useLocation,
+  useNavigate,
+} from '#libs/hooks/hooks.js';
+import { StorageKey } from '#libs/packages/storage/storage.js';
+import { actions as authActions } from '#slices/auth/auth.js';
 import { actions as userActions } from '#slices/users/users.js';
 
 const App: React.FC = () => {
   const { pathname } = useLocation();
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { users, dataStatus, user, userDataStatus } = useAppSelector(
+    ({ users, auth }) => ({
+      users: users.users,
+      dataStatus: users.dataStatus,
+      user: auth.user,
+      userDataStatus: auth.dataStatus,
+    }),
+  );
 
   const isRoot = pathname === AppRoute.ROOT;
 
@@ -15,6 +32,17 @@ const App: React.FC = () => {
       void dispatch(userActions.loadAll());
     }
   }, [isRoot, dispatch]);
+
+  useEffect(() => {
+    if (!localStorage.getItem(StorageKey.TOKEN)) {
+      navigate(AppRoute.SIGN_IN);
+    }
+    void dispatch(authActions.getUser())
+      .unwrap()
+      .catch(() => {
+        navigate(AppRoute.SIGN_IN);
+      });
+  }, []);
 
   return (
     <>
@@ -37,6 +65,19 @@ const App: React.FC = () => {
       <div>
         <RouterOutlet />
       </div>
+      {isRoot && (
+        <>
+          <h2>User: {user?.email}</h2>
+          <h2>User Status: {userDataStatus}</h2>
+          <h2>Users:</h2>
+          <h3>Status: {dataStatus}</h3>
+          <ul>
+            {users.map((user) => (
+              <li key={user.id}>{user.email}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 };
